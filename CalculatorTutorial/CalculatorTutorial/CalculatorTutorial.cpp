2e26fc31ae53ce8e00940adea11822a432950213
Yury <47724439+Yura-63@users.noreply.github.com>
2021-03-05 12:19:44 +0400

    Загрузка калькулятора
    Загрузил то, что пока есть.

---
+++ CalculatorTutorial/CalculatorTutorial/CalculatorTutorial.cpp
@@ -0,0 +1,37 @@
+﻿// CalculatorTutorial.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "Calculator.h"
+
+using namespace std;
+
+int main()
+{
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+
+    setlocale(LC_ALL, "Russian");
+    cout << "Консольное приложение калькулятора" << endl << endl;
+    cout << "Введите операцию, которую нужно выполнить: a+b | a-b | a*b | a/b"<< endl;
+
+    Calculator c;
+    while (true)
+    {
+        cin >> x >> oper >> y;
+        if (oper == '/' && y == 0)
+        {
+            cout << "Деление на ноль не допустимо. " << endl;
+            continue;
+        }
+        else
+        {
+            result = c.Calculate(x, oper, y);
+        }
+        cout << "Результат: " << result << "\n" << endl;
+    }
+
+    return 0;
+}
